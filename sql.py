ea3123365a84dca5f2eca763ea2d0b85203eb99b
Md Alam <alamwebtech@gmail.com>
2019-02-14 12:41:22 -0500

    created table

--- sql.py
+++ sql.py
@@ -3,7 +3,10 @@ conn = sqlite3.connect("my_friends.db")
 #Create cursor object
 c = conn.cursor()
 # execute some sql
-c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+# c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+# insert_query = ''' INSERT INTO friends
+#                      VALUES ('Merriwether', 'Lewis', 7)'''
+c.execute(insert_query)
 #commit changes
 conn.commit()
 conn.close()
